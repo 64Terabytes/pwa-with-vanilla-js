9d67b74215fcba543526b913c28d9c55de92452a
64Terabytes <jamin.pepper15@gmail.com>
2021-03-05 03:25:08 +0000

    Whole new site basicly

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,16 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticACMusic = "AC-Music-PWA-v1";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/images/new-leaf-box-art.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
+    caches.open(staticACMusic).then(cache => {
+       return cache.addAll(assets);
     })
   );
 });
