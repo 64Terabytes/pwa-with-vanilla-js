d4152c341812e72238492d18df1897641e980d93
64Terabytes <jamin.pepper15@gmail.com>
2021-03-05 04:30:52 +0000

    Comment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 const container = document.querySelector(".container");
+var x = document.createElement("AUDIO");
 
-// document.addEventListener("DOMContentLoaded", showCoffees);
+function Playsong(file) {
+    x.src=file;
+    x.play();
+}
+function StartApp() {
+    Playsong("/audio/1 AM.mp3");
+}
+
+
+document.addEventListener("DOMContentLoaded", StartApp);
+document.querySelector("#NewLeafArt")
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
